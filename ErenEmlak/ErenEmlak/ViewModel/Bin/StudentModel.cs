974f96995dcab3badfdec10521f019ece5ac1445
ErenBakkal <122541834+ErenBakkal@users.noreply.github.com>
2023-05-08 20:39:52 +0300

    WebApiİntProj-2Vize

---
+++ ErenEmlak/ErenEmlak/ViewModel/Bin/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ErenEmlak.ViewModel
+{
+    public class StudentModel
+    {
+        public string StudentId { get; set; }
+        public string StudentNo { get; set; }
+        public string StudentFullName { get; set; }
+        public string StudentBirthDate { get; set; }
+    }
+}
